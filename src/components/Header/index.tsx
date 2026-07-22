f6d5122b1c6bc182719e5f0f8787f9bb6197e238
Gabriel Oliveira <gabrieloliveirag403@gmail.com>
2026-07-22 19:20:46 -0300

    feat: adiciona drawer do carrinho de compras

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import Logo from "../../assets/images/logo.png";
 import IconUser from "../../assets/images/icon-user.png";
 import IconAbout from "../../assets/images/icon-about.png";
-import IconCart from "../../assets/images/icon-cart.png";
 import { Link } from "@tanstack/react-router";
+import ShoppingCart from "../ShoppingCart";
 
 const Header = () => {
   return (
@@ -46,9 +46,11 @@ const Header = () => {
                 </a>
               </li>
               <li>
-                <a href="#">
+                {/* <a href="#">
                   <img src={IconCart} alt="Ícone de Carrinho" />
-                </a>
+                </a> */}
+
+                <ShoppingCart />
               </li>
             </ul>
           </nav>
